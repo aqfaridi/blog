8384f2eaa1225a0926fc344defb51ff08df3b096
Abdul Qadir Faridi <aqfaridi@gmail.com>
2016-06-22 16:57:52 +0530

    Devise user authentication done with debugging gems used

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,25 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  text       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  slug       :string
+#  user_id    :integer
+#
+# Indexes
+#
+#  index_articles_on_slug  (slug)
+#
+
 class Article < ActiveRecord::Base
-	extend FriendlyId
+  extend FriendlyId
   friendly_id :title, use: [:slugged, :finders, :history]
 
+  belongs_to :user 
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
