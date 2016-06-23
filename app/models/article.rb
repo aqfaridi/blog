5108b475caa1d582769f7fa5dc5a5ec0d6414167
Abdul Qadir Faridi <aqfaridi@gmail.com>
2016-06-23 14:44:28 +0530

    more developer gems configured

--- app/models/article.rb
+++ app/models/article.rb
@@ -18,13 +18,12 @@
 class Article < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: [:slugged, :finders, :history]
-
-  belongs_to :user 
+  belongs_to :user
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
 
   def should_generate_new_friendly_id?
 		title_changed?
-  end                  
+  end
 end
