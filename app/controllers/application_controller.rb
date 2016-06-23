6c502e4364665a4014ca90fef1bc414e39e91872
Abdul Qadir Faridi <aqfaridi@gmail.com>
2016-06-23 10:18:50 +0530

    bug fixed with Edit and Destroy button verification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def validation(user,article)
+    unless user.email == article.user.email
+      redirect_to article
+    end
+  end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,7 @@ class CommentsController < ApplicationController
   
   def destroy
     @article = Article.find(params[:article_id])
+    validation(current_user,@article)
     @comment = @article.comments.find(params[:id])
     @comment.destroy
     redirect_to article_path(@article)
@@ -18,4 +19,5 @@ class CommentsController < ApplicationController
     def comment_params
       params.require(:comment).permit(:commenter, :body)
   end
+
 end
